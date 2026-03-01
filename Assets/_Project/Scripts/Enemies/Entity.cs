5b238047e09dd8c2c52fd5620ec7a70be1606ac2
Kirill Dovganiuk <dovganiuk.ka@phystech.edu>
2026-03-01 22:15:02 +0300

    Imput system is implemented, movement is working, transform for aiming is also working

---
+++ Assets/_Project/Scripts/Enemies/Entity.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Entity: MonoBehaviour
+{
+    public float MoveSpeed;
+    public Transform aimPosition; // Transform, в чью сторону смотрит Entity
+
+    public void moveTo(Vector2 dir) // переместиться в направлении dir
+    {
+        dir *= Time.deltaTime * MoveSpeed;
+        transform.localPosition = new Vector3(transform.position.x + dir.x, 
+                                         transform.position.y + dir.y,  
+                                         transform.position.z);
+    }
+
+    public void lookAt(Vector3 coords) // посмотреть на объект с координатами coords
+    {
+        Vector3 localPosition = coords - transform.position; 
+        aimPosition.localPosition = new Vector3(localPosition.x, 
+                                           localPosition.y,  
+                                           transform.position.z);
+    }
+}
---
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController: MonoBehaviour
+{
+    public InputActionAsset InputActions;
+
+    private InputAction m_moveAction;
+    private InputAction m_lookAction;
+    private InputAction m_interactAction;
+
+    public Entity player;
+    private Vector2 m_moveAmt;
+    private Vector2 m_lookAmt; // в координатах мира, используя основную камеру
+
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+
+    private void Awake()
+    {
+        m_moveAction = InputSystem.actions.FindAction("Move");
+        m_lookAction = InputSystem.actions.FindAction("Look");
+        m_interactAction = InputSystem.actions.FindAction("Interact");
+    }
+
+    private void Update()
+    {
+        m_moveAmt = m_moveAction.ReadValue<Vector2>();
+        m_lookAmt = Camera.main.ScreenToWorldPoint(m_lookAction.ReadValue<Vector2>());
+
+        if (m_interactAction.WasPressedThisFrame())
+        {
+            Interact();
+        }
+    }
+
+    private void Interact()
+    {
+        Debug.Log("Action: Interract");
+
+        // ...
+    }
+
+    private void FixedUpdate()
+    {
+        Walking();
+        Looking();
+    }
+
+    private void Walking()
+    {
+        player.moveTo(m_moveAmt);
+    }
+
+    private void Looking()
+    {
+        player.lookAt(m_lookAmt);
+    }
+}
