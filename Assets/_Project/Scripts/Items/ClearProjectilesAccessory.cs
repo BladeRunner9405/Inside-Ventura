383c7326f9e15d4687d8f057fadd4b46e12586d5
Русин Антон Владимирович <rusin.av@phystech.edu>
2026-03-05 03:17:59 +0300

    implement ClearProjectiles accessory

--- Assets/_Project/Scripts/Items/ClearProjectilesAccessory.cs
+++ Assets/_Project/Scripts/Items/ClearProjectilesAccessory.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewClearProjectilesAccessory",
   menuName = "Inside-Ventura/Artifacts/Accessory/ClearProjectiles")]
 public class ClearProjectilesAccessory : Accessory {
+  [SerializeField] private LayerMask projectileLayer;
   [SerializeField] private float radius = 3f;
 
   protected override void UseAbility(GameObject playerObject, Vector2 direction) {
+    var hits = Physics2D.OverlapCircleAll(playerObject.transform.position, radius, projectileLayer);
+    foreach (var hit in hits) Destroy(hit.gameObject);
   }
 }
