684f35f1c7bbc03939bdbcf32771ca789ee3db50
Roman Kabirov <roman.kabirov.2003@mail.ru>
2026-03-08 11:37:30 +0300

    fix git ignoring assets issue

---
+++ Assets/_Project/Scripts/World/DungeonManagement/DungeonManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using Edgar.Unity;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Debug = UnityEngine.Debug;
+
+public class DungeonManager : MonoBehaviour {
+  public System.Random Random { get; private set; }
+
+  public InputActionAsset inputActions;
+
+  public static DungeonManager Instance;
+
+  private void OnEnable() {
+    Debug.Log("Enabling input acions...");
+    inputActions.Enable();
+  }
+
+  private void OnDisable() {
+    Debug.Log("Disabling input acions...");
+    inputActions.Enable();
+  }
+
+  public void Awake() {
+    Random = new();
+
+    if (Instance == null) {
+      Instance = this;
+    }
+
+    // Find the generator runner
+    var generator = GameObject.Find("Dungeon Generator").GetComponent<DungeonGeneratorGrid2D>();
+
+    // Start the generator coroutine
+    StartCoroutine(GeneratorCoroutine(generator));
+  }
+
+  /// <summary>
+  /// Coroutine that generates the level.
+  /// We need to yield return before the generator starts because we want to show the loading screen
+  /// and it cannot happen in the same frame.
+  /// It is also sometimes useful to yield return before we hide the loading screen to make sure that
+  /// all the scripts that were possibly created during the process are properly initialized.
+  /// </summary>
+  private IEnumerator GeneratorCoroutine(DungeonGeneratorGrid2D generator) {
+    var stopwatch = new Stopwatch();
+
+    stopwatch.Start();
+
+    yield return null;
+
+    generator.Generate();
+
+    yield return null;
+
+    stopwatch.Stop();
+  }
+}
---
+++ Assets/_Project/Scripts/World/DungeonManagement/RoomManager.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Edgar.Unity;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour {
+  /// <summary>
+  /// Enemies that can spawn inside the room.
+  /// </summary>
+  private GameObject[] enemyPrefabs;
+
+  private int remainingEnemiesCount;
+
+  private List<GameObject> doors;
+
+  /// <summary>
+  /// Whether enemies were spawned.
+  /// </summary>
+  private bool enemiesSpawned;
+
+  /// <summary>
+  /// Collider of the floor tilemap layer.
+  /// </summary>
+  private Collider2D floorCollider;
+
+
+  /// <summary>
+  /// Room instance of the corresponding room.
+  /// </summary>
+  private RoomInstanceGrid2D roomInstance;
+
+  public void Init(GameObject[] enemyPrefabs, Collider2D floorCollider,
+    RoomInstanceGrid2D roomInstance) {
+    this.enemyPrefabs = enemyPrefabs;
+    this.floorCollider = floorCollider;
+    this.roomInstance = roomInstance;
+
+    doors = new();
+    foreach (var door in roomInstance.Doors) {
+      // Get the room instance of the room that is connected via this door
+      var corridorRoom = door.ConnectedRoomInstance;
+
+      // Get the room template instance of the corridor room
+      var corridorGameObject = corridorRoom.RoomTemplateInstance;
+
+      // Find the door game object by its name
+      var doorsGameObject = corridorGameObject.transform.Find("Door")?.gameObject;
+
+      if (doorsGameObject != null) {
+        doorsGameObject.SetActive(false);
+        doors.Add(doorsGameObject);
+      }
+      else {
+        Debug.Log("No doors in the corridor, are you sure?");
+      }
+    }
+  }
+
+  /// <summary>
+  /// Use the shared Random instance so that the results are properly seeded.
+  /// </summary>
+  private static System.Random Random => DungeonManager.Instance.Random;
+
+  /// <summary>
+  /// Gets called when a player enters the room.
+  /// </summary>
+  /// <param name="player"></param>
+  public void OnRoomEnter(GameObject player) {
+    Debug.Log(
+      $"Room enter. Room name: {roomInstance.Room.GetDisplayName()}, Room template: {roomInstance.RoomTemplatePrefab.name}");
+
+    var type = ((DungeonRoom)roomInstance.Room).type;
+    if (roomInstance.IsCorridor || type != DungeonRoomType.Normal || enemiesSpawned) return;
+
+    Debug.Log("Spawning enemies and closing all doors...");
+    SpawnEnemies();
+    foreach (var door in doors) {
+      door.SetActive(true);
+    }
+  }
+
+  /// <summary>
+  /// Gets called when a player leaves the room.
+  /// </summary>
+  /// <param name="player"></param>
+  public void OnRoomLeave(GameObject player) {
+    Debug.Log($"Room leave {roomInstance.Room.GetDisplayName()}");
+  }
+
+
+  private void SpawnEnemies() {
+    enemiesSpawned = true;
+
+    var enemies = new List<Enemy>();
+    var totalEnemiesCount = Random.Next(4, 8);
+
+    while (enemies.Count < totalEnemiesCount) {
+      // Find random position inside floor collider bounds
+      var position = RandomPointInBounds(floorCollider.bounds, 1f);
+
+      // Check if the point is actually inside the collider as there may be holes in the floor, etc.
+      if (!IsPointWithinCollider(floorCollider, position)) {
+        continue;
+      }
+
+      // We want to make sure that there is no other collider in the radius of 1
+      if (Physics2D.OverlapCircleAll(position, 0.5f).Any(x => !x.isTrigger)) {
+        continue;
+      }
+
+      // Pick random enemy prefab
+      var enemyPrefab = enemyPrefabs[Random.Next(0, enemyPrefabs.Length)];
+
+      // Create an instance of the enemy and set position and parent
+      var enemy = Instantiate(enemyPrefab, roomInstance.RoomTemplateInstance.transform, true);
+      enemy.transform.position = position;
+
+      var gungeonEnemy = enemy.GetComponent<Enemy>();
+      gungeonEnemy.OnDeath += () => {
+        Debug.Log($"Enemy died, remaining enemies: {--remainingEnemiesCount}");
+
+        if (remainingEnemiesCount != 0) return;
+
+        foreach (var door in doors) {
+          door.SetActive(false);
+        }
+      };
+      enemies.Add(gungeonEnemy);
+    }
+
+    remainingEnemiesCount = totalEnemiesCount;
+  }
+
+  private static bool IsPointWithinCollider(Collider2D collider, Vector2 point) {
+    return collider.OverlapPoint(point);
+  }
+
+  private static Vector3 RandomPointInBounds(Bounds bounds, float margin = 0) {
+    return new Vector3(
+      RandomRange(bounds.min.x + margin, bounds.max.x - margin),
+      RandomRange(bounds.min.y + margin, bounds.max.y - margin),
+      RandomRange(bounds.min.z + margin, bounds.max.z - margin)
+    );
+  }
+
+  private static float RandomRange(float min, float max) {
+    return (float)(Random.NextDouble() * (max - min) + min);
+  }
+}
