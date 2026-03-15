ffcc649ab070b54ec3d2abd0faa3a6dc9a035be5
Roman Kabirov <roman.kabirov.2003@mail.ru>
2026-03-15 13:53:49 +0300

    refactor: move attack behaviour from UA class to UAAttackField

--- Assets/_Project/Scripts/Enemies/UncontrollableAnger/UncontrollableAnger.cs
+++ Assets/_Project/Scripts/Enemies/UncontrollableAnger/UncontrollableAnger.cs
@@ -1,11 +1,10 @@
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
+
 [RequireComponent(typeof(Animator))]
-public class UncontrollableRage : Enemy {
+public class UncontrollableAnger : Enemy {
   private enum State {
     Walking,
     Attacking,
@@ -19,23 +18,19 @@ public class UncontrollableRage : Enemy {
 
   [Header("Attack"), SerializeField] private float hitboxWidth = 1F;
   [SerializeField] private float hitboxesHeight = 1.2F;
-  [SerializeField] private List<BoxCollider2D> hitboxes = new();
-  [SerializeField] private float attackWaveDuration = 0.5F;
+  [SerializeField] private GameObject attackField;
 
+  [Header("Advanced"), SerializeField, Tooltip("Distance that enemy uses to find a place to retreat")]
+  private float retreatLookupDistance = 0.2f;
 
-  [Header("Advanced"), SerializeField] private float retreatLookupDistance = 0.2f;
-
-  private Transform _hitboxParent;
   private State _state = State.Walking;
   private float _curCooldown;
-  private int _curHitbox = -1;
+
   private Animator _animator;
 
   protected new void Start() {
     base.Start();
-
     _animator = GetComponent<Animator>();
-    _hitboxParent = hitboxes[0].transform.parent;
   }
 
   private void Update() {
@@ -63,14 +58,10 @@ public class UncontrollableRage : Enemy {
         ResolveWalkingDirection();
         TryAttack();
         break;
-    }
-
-    // Transform attack wave to look at player.
-    if (_state != State.Attacking) {
-      var diff = target.position - transform.position;
-      diff.Normalize();
-      var rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-      _hitboxParent.rotation = Quaternion.Euler(0f, 0f, rotZ + 90);
+      case State.Attacking:
+        Agent.SetDestination(transform.position);
+        Agent.isStopped = true;
+        break;
     }
   }
 
@@ -78,40 +69,25 @@ public class UncontrollableRage : Enemy {
     var dist = Vector3.Distance(target.position, transform.position);
     if (dist > playerDistance || _curCooldown > 0.0F) return;
 
-    Agent.isStopped = true;
     _state = State.Attacking;
     _animator.SetTrigger("StartAttacking");
   }
 
   // ReSharper disable once InconsistentNaming
-  // Called from UA animation clip.
+  // Called from UA animation clip. Spawn the attack field.
   private void UADoDamage() {
-    _curHitbox = 0;
-    DOTween.Sequence().AppendCallback(() => { Debug.Log("Wave sequence started"); })
-      .Append(DOTween.To(() => _curHitbox, UpdateHitbox, 3, attackWaveDuration))
-      .AppendCallback(() => {
-        _curHitbox = -1;
-        Debug.Log("Wave sequence ended");
-      }).PlayForward();
-  }
+    var diff = target.position - transform.position;
+    diff.Normalize();
+    var rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+    var rot = Quaternion.Euler(0f, 0f, rotZ + 90);
 
-  private void UpdateHitbox(int newVal) {
-    if (newVal == _curHitbox) {
-      return;
-    }
-
-    Debug.Log($"Updating hitbox with new index {newVal}");
-    _curHitbox = newVal;
-    if (hitboxes[_curHitbox].OverlapPoint(target.position)) {
-      PlayerAccessor.Player.TakeDamage(damage);
-    }
+    Instantiate(attackField, transform.position, rot).GetComponent<UAAttackField>().damage = damage;
   }
 
   // ReSharper disable once InconsistentNaming
   // Called from UA animation clip.
   private void UAStartCooldown() {
     _curCooldown = cooldownDuration;
-    Agent.isStopped = false;
     ResolveWalkingDirection();
   }
 
@@ -125,9 +101,11 @@ public class UncontrollableRage : Enemy {
     Gizmos.color = Color.white;
     Gizmos.DrawWireSphere(transform.position, playerDistance);
 
-    Gizmos.color = Color.yellow;
-    var dir = (transform.position - target.position).normalized;
-    Gizmos.DrawLine(transform.position, transform.position + dir * retreatLookupDistance * 5);
+    if (target != null) {
+      Gizmos.color = Color.yellow;
+      var dir = (transform.position - target.position).normalized;
+      Gizmos.DrawLine(transform.position, transform.position + dir * retreatLookupDistance * 5);
+    }
 
     Gizmos.DrawWireSphere(transform.position, retreatDistance);
   }
@@ -135,13 +113,6 @@ public class UncontrollableRage : Enemy {
   private void OnDrawGizmos() {
     if (_curCooldown > 0.0F) {
       UnityEditor.Handles.Label(transform.position, $"Cooldown:  {_curCooldown}\nState: {_state}");
-    }
-
-    if (_curHitbox != -1) {
-      var curCollider = hitboxes[_curHitbox];
-      Gizmos.matrix = Matrix4x4.TRS(_hitboxParent.transform.position, _hitboxParent.transform.rotation, Vector3.one);
-      Gizmos.color = Color.red;
-      Gizmos.DrawWireCube(curCollider.transform.localPosition, new Vector3(hitboxWidth, hitboxWidth, 0));
     }
   }
 }
