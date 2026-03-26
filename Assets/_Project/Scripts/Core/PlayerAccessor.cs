44ee3e909cc300773e913e6ed0301ec0d4b268eb
Русин Антон Владимирович <rusin.av@phystech.edu>
2026-03-26 15:47:15 +0300

    implement invulnerability frames

--- Assets/_Project/Scripts/Core/PlayerAccessor.cs
+++ Assets/_Project/Scripts/Core/PlayerAccessor.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class PlayerAccessor : IPlayerData {
   private Player _player;
+  public PlayerInventory Inventory => _player?.Inventory;
+
+  public Transform Transform => _player?.transform;
+  public PlayerEquipment Equipment => _player?.Equipment;
+  public PlayerStats Stats => _player?.Stats;
+
+  public Stat GetStat(StatName statName) {
+    return _player?.GetStat(statName);
+  }
+
+  public void Dash(Vector2 direction, float distance, float duration) {
+    _player?.Dash(direction, distance, duration);
+  }
 
   public void RegisterPlayer(Player player) {
     _player = player;
@@ -10,20 +23,6 @@ public class PlayerAccessor : IPlayerData {
   public void UnregisterPlayer(Player player) {
     if (_player == player)
       _player = null;
-  }
-
-  public Transform Transform => _player?.transform;
-  public PlayerEquipment Equipment => _player?.Equipment;
-  public PlayerInventory Inventory => _player?.Inventory;
-  public PlayerStats Stats => _player?.Stats;
-
-  public Stat GetStat(StatName statName) {
-    return _player?.GetStat(statName);
-  }
-
-  public void Dash(Vector2 direction, float distance, float duration)
-  {
-    _player?.Dash(direction, distance, duration);
   }
 
   public void TakeDamage(float amount) {
