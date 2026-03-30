58b0208c8d5e27e9dda2b23686f0147b3ff838e3
Roman Kabirov <roman.kabirov.2003@mail.ru>
2026-03-30 15:23:31 +0300

    fix doors locking after clearing room cleanup code

--- Assets/_Project/Scripts/World/DungeonManagement/RoomManagers/NormalRoomManager.cs
+++ Assets/_Project/Scripts/World/DungeonManagement/RoomManagers/NormalRoomManager.cs
@@ -7,20 +7,10 @@ public class NormalRoomManager : RoomManagerBase {
   private List<GameObject> _doors;
 
   private int _waveCount;
-  private List<GameObject> _waves = new List<GameObject>();
+  private List<GameObject> _waves = new();
   private int _curWave = -1;
 
   private GameObject _chest;
-
-  /// <summary>
-  /// Collider of the floor tilemap layer.
-  /// </summary>
-  private Collider2D _floorCollider;
-
-  /// <summary>
-  /// Enemies that can spawn inside the room.
-  /// </summary>
-  private GameObject[] _enemyPrefabs;
 
   private int _remainingEnemiesCount;
 
@@ -31,21 +21,22 @@ public class NormalRoomManager : RoomManagerBase {
 
   public override void Init(Collider2D floorCollider, RoomInstanceGrid2D roomInstance) {
     base.Init(floorCollider, roomInstance);
-
-    _floorCollider = floorCollider;
     _doors = DetectDoors(roomInstance);
 
-    _chest = transform.Find("Chest").gameObject;
+    _chest = transform.Find("Chest")?.gameObject;
     if (_chest == null) {
       Debug.Log("No chest inside the room, are you sure?");
     }
-    _chest.SetActive(false);
+    _chest?.SetActive(false);
 
-    var wavesRoot = transform.Find("Waves").gameObject;
-    wavesRoot.SetActive(true);
+    var wavesRoot = transform.Find("Waves")?.gameObject;
     if (wavesRoot == null) {
       Debug.Log("No Waves inside the room, are you sure?");
+      _enemiesSpawned = true;
+      return;
     }
+
+    wavesRoot.SetActive(true);
 
     // Detect waves.
     while (true) {
@@ -89,11 +80,10 @@ public class NormalRoomManager : RoomManagerBase {
     if (RoomInstance == null) return;
     base.OnRoomEnter(player);
 
-    Debug.Log("Spawning enemies and closing all doors...");
-    foreach (var door in _doors) {
-      door.SetActive(true);
-    }
+    if (_enemiesSpawned) return;
 
+    Debug.Log("Spawning enemies and closing all doors...");
+    DoorsSetActive(true);
     AdvanceWave();
   }
 
@@ -101,9 +91,8 @@ public class NormalRoomManager : RoomManagerBase {
     _curWave++;
     if (_curWave == _waveCount) {
       SpawnChest();
-      foreach (var door in _doors) {
-        door.SetActive(false);
-      }
+      DoorsSetActive(false);
+      _enemiesSpawned = true;
       return;
     }
 
@@ -123,68 +112,13 @@ public class NormalRoomManager : RoomManagerBase {
     }
   }
 
+  private void DoorsSetActive(bool active) {
+    foreach (var door in _doors) {
+      door.SetActive(active);
+    }
+  }
+
   private void SpawnChest() {
     _chest.SetActive(true);
-  }
-
-  private void SpawnEnemies() {
-    _enemiesSpawned = true;
-
-    var enemies = new List<Enemy>();
-    var totalEnemiesCount = Random.Next(1, 3);
-    Debug.Log(_floorCollider.bounds);
-    while (enemies.Count < totalEnemiesCount) {
-      // Find random position inside floor collider bounds
-      var position = RandomPointInBounds(_floorCollider.bounds, 1f);
-
-      // Check if the point is actually inside the collider as there may be holes in the floor, etc.
-      if (!IsPointWithinCollider(_floorCollider, position)) {
-        continue;
-      }
-
-      // We want to make sure that there is no other collider in the radius of 1
-      if (Physics2D.OverlapCircleAll(position, 0.5f).Any(x => !x.isTrigger)) {
-        continue;
-      }
-
-      // Pick random enemy prefab
-      var enemyPrefab = _enemyPrefabs[Random.Next(0, _enemyPrefabs.Length)];
-
-      // Create an instance of the enemy and set position and parent
-      var enemy = Instantiate(enemyPrefab, RoomInstance.RoomTemplateInstance.transform, true);
-      enemy.transform.position = position;
-      Debug.Log(position);
-
-      var gungeonEnemy = enemy.GetComponent<Enemy>();
-      gungeonEnemy.OnDeath += () => {
-        Debug.Log($"Enemy died, remaining enemies: {--_remainingEnemiesCount}");
-
-        if (_remainingEnemiesCount != 0) return;
-
-        foreach (var door in _doors) {
-          door.SetActive(false);
-        }
-      };
-      enemies.Add(gungeonEnemy);
-    }
-
-    _remainingEnemiesCount = totalEnemiesCount;
-  }
-
-  private static bool IsPointWithinCollider(Collider2D collider, Vector2 point) {
-    return collider.OverlapPoint(point);
-  }
-
-  private static Vector3 RandomPointInBounds(Bounds bounds, float margin = 0) {
-    return new Vector3(
-      RandomRange(bounds.min.x + margin, bounds.max.x - margin),
-      RandomRange(bounds.min.y + margin, bounds.max.y - margin),
-      // RandomRange(bounds.min.z + margin, bounds.max.z - margin)
-      0
-    );
-  }
-
-  private static float RandomRange(float min, float max) {
-    return (float)(Random.NextDouble() * (max - min) + min);
   }
 }
