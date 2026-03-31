3afd808d0098f8f85886f8b250c7abf9bce36eb1
Русин Антон Владимирович <rusin.av@phystech.edu>
2026-03-31 20:42:10 +0300

    disabling Player action map in menu

---
+++ Assets/_Project/Scripts/UI/InputToggler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class InputToggler : MonoBehaviour
+{
+  [SerializeField] private InputActionAsset inputActionAsset;
+  [SerializeField] private string actionMapName = "Player";
+
+  private Button _button;
+  private InputActionMap _playerActionMap;
+
+  private bool _isActive = true;
+
+  private void Start() {
+    _button = GetComponent<Button>();
+    if (_button != null) _button.onClick.AddListener(TogglePanel);
+
+    if (inputActionAsset != null)
+      _playerActionMap = inputActionAsset.FindActionMap(actionMapName);
+  }
+
+  private void OnDestroy() {
+    if (_button != null) _button.onClick.RemoveListener(TogglePanel);
+  }
+
+  private void TogglePanel() {
+    if (_playerActionMap != null) {
+      if (_isActive)
+        _playerActionMap.Disable();
+      else
+        _playerActionMap.Enable();
+      _isActive = !_isActive;
+    }
+  }
+}
