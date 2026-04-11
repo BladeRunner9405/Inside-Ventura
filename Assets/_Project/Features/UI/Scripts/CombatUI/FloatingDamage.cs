4d4c848852d5223cae47be5a049176c5ce89965b
Roman Kabirov <roman.kabirov.2003@mail.ru>
2026-04-11 16:51:39 +0300

    animation

--- Assets/_Project/Features/UI/Scripts/CombatUI/FloatingDamage.cs
+++ Assets/_Project/Features/UI/Scripts/CombatUI/FloatingDamage.cs
@@ -1,13 +1,24 @@
 using System.Globalization;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
+
 public class FloatingDamage : MonoBehaviour {
   [SerializeField] private TMP_Text text;
+  [SerializeField] private float duration = 0.3F;
+  [SerializeField] private float upMoveAmount = 1;
 
   public void Initialize(float damage) {
     text.text = damage.ToString(CultureInfo.InvariantCulture);
-    Invoke(nameof(Disappear), 0.3F);
+    text.alpha = 1;
+
+    transform.DOMoveY(upMoveAmount, duration);
+    DOTween.To(() => transform.position, p => transform.position = p, transform.position + Vector3.up * upMoveAmount,
+      duration);
+    DOTween.To(() => text.alpha, x => text.alpha = x, 0, duration);
+
+    Invoke(nameof(Disappear), duration);
   }
 
   private void Disappear() {
